d22d817e004bdd650e308eaf5759eb7ee85f0007
santhosh2566 <sansanthosh2566@gmail.com>
2026-03-05 01:30:44 -0800

    https://profile-mern-frontend.vercel.app

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173", 
-    credentials: true,
-  })
-);
+  origin: [
+    "http://localhost:5173",
+    "https://profile-mern-frontend.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
