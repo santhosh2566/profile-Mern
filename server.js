a9ab92770304ddec460ec1b6ae4181003c5680d4
santhosh2566 <sansanthosh2566@gmail.com>
2026-03-05 21:08:13 -0800

    fix by sd

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
@@ -26,8 +27,6 @@ app.use("/api/profile", profileRoutes);
 app.get("/", (req, res) => {
   res.send("🚀 API is running...");
 });
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
